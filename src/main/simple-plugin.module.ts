feb2bde9bb973920a5be929eb230e70702e7ffd2
Ryan Bradford <rbradford@vmware.com>
2020-01-24 13:41:28 -0500

    Moved some code to the core components library

--- src/main/simple-plugin.module.ts
+++ src/main/simple-plugin.module.ts
@@ -3,13 +3,12 @@ import { Inject, NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { Store } from "@ngrx/store";
 import { VcdApiClient, VcdSdkModule } from "@vcd/sdk";
-import { ExtensionNavRegistration, EXTENSION_ROUTE } from "@vcd/sdk/common";
+import { ExtensionNavRegistration, EXTENSION_ROUTE, EXTENSION_ASSET_URL } from "@vcd/sdk/common";
 import { PluginModule } from "@vcd/sdk/core";
 import { TranslateService } from "@vcd/sdk/i18n";
 import { ClarityModule } from "@clr/angular";
 import { SimpleComponent } from "./simple/simple.component";
-import { I18nModule, PLUGIN_TRANSLATION_SERVICE } from "./translation/I18n.module";
-import { TranslationService, BOOTSTRAP_DETAILS } from "@vcd/ui-components";
+import { TranslationService, I18nModule } from "@vcd/ui-components";
 import { DataExporterExamplesModule } from "./test/data-exporter.examples.module";
 
 const ROUTES: Routes = [
@@ -22,7 +21,7 @@ const ROUTES: Routes = [
         CommonModule,
         VcdSdkModule,
         RouterModule.forChild(ROUTES),
-        I18nModule,
+        I18nModule.forChild(EXTENSION_ASSET_URL, true),
         DataExporterExamplesModule
     ],
     declarations: [
@@ -33,9 +32,9 @@ const ROUTES: Routes = [
     providers: [VcdApiClient]
 })
 export class SimplePluginModule extends PluginModule {
-    constructor(appStore: Store<any>, @Inject(EXTENSION_ROUTE) extensionRoute: string, translate: TranslateService,
-    @Inject(PLUGIN_TRANSLATION_SERVICE) pluginTranslate: TranslationService) {
+    constructor(appStore: Store<any>, @Inject(EXTENSION_ROUTE) extensionRoute: string, translate: TranslateService, pluginTranslate: TranslationService) {
         super(appStore, translate);
+        console.log(extensionRoute);
         pluginTranslate.registerTranslations()
         this.registerExtension(<ExtensionNavRegistration>{
             path: extensionRoute,
